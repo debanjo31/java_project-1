6be9dd5ee33a6b0777fc3dee88af195776d0e2a2
Adebanjo Israel <debanjo31@gmail.com>
2024-01-01 01:18:24 +0100

    updated

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+//        Scanner sc =   new Scanner(System.in);
+//        System.out.print("What is the temperature of your water? :  ");
+//        int temperature = sc.nextInt();
+//        if(temperature >= 100){
+//            System.out.println("Boiling");
+//        }
+//        else {
+//            System.out.println("Not Boiling");
+//        }
+        Animal dog = new Animal();
+        dog.bark();
+    }
+}
